2edf2765a0dcc32d1eb8489e4ba68095a469e495
Henrique M. V. dos Santos <henriquemvsantos@hotmail.com>
2022-08-20 13:49:37 -0300

    ajustes program da web api

--- MassTransitProject/Program.cs
+++ MassTransitProject/Program.cs
@@ -5,21 +5,32 @@ using MassTransitProject.Contracts;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+#region MassTransitConfigWebApi
 var services = builder.Services;
 services.AddSingleton(KebabCaseEndpointNameFormatter.Instance);
-
 builder.Services.AddMassTransit(cfg =>
 {
-    cfg.AddConsumer<SubmitOrderConsumer>();
-    cfg.AddMediator();
-
-    cfg.UsingInMemory((context, cfg) =>
-    {
-        cfg.ConfigureEndpoints(context);
-    });
-    //cfg.UsingRabbitMq();
+    cfg.UsingRabbitMq();
     cfg.AddRequestClient<SubmitOrder>();
 });
+
+services.AddOptions<MassTransitHostOptions>()
+    .Configure(options =>
+    {
+        // if specified, waits until the bus is started before
+        // returning from IHostedService.StartAsync
+        // default is false
+        options.WaitUntilStarted = true;
+
+        // if specified, limits the wait time when starting the bus
+        options.StartTimeout = TimeSpan.FromSeconds(10);
+
+        // if specified, limits the wait time when stopping the bus
+        options.StopTimeout = TimeSpan.FromSeconds(30);
+    });
+//services.AddMassTransitHostedService();
+#endregion
+//builder.Services.AddScoped<SubmitOrderConsumer>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
